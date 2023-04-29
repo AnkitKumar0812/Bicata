f70fda8c245b833010a6a3257fa69642dacf272e
Ankit gupta <ankitkumar89692@gmail.com>
2023-04-29 20:59:04 +0530

    29/04/2023

--- app/src/main/java/com/example/bicatakotline/SplashScreenActivity.kt
+++ app/src/main/java/com/example/bicatakotline/SplashScreenActivity.kt
@@ -3,16 +3,74 @@ package com.example.bicatakotline
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.firebase.ui.auth.AuthMethodPickerLayout
+import com.firebase.ui.auth.AuthUI
+import com.google.android.gms.auth.api.Auth
+import com.google.firebase.auth.FirebaseAuth
 import io.reactivex.Completable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import kotlinx.coroutines.delay
+import java.util.Arrays
 import java.util.concurrent.TimeUnit
 
 class SplashScreenActivity : AppCompatActivity() {
+    companion object{
+        private val LOGIN_REQUEST_CODE = 7171
+    }
+    private lateinit var providers: List<AuthUI.IdpConfig>
+    private  lateinit var firebaseAuth: FirebaseAuth
+    private  lateinit var listener:FirebaseAuth.AuthStateListener
+
+    override fun onStart(){
+        super.onStart()
+        delaySplashScreen();
+    }
+    override fun onStop(){
+        if(firebaseAuth != null && listener != null) firebaseAuth.removeAuthStateListener(listener)
+        super.onStop()
+    }
+
+    private fun delaySplashScreen() {
+        Completable.timer(3, TimeUnit.SECONDS,AndroidSchedulers.mainThread())
+            .subscribe({
+                firebaseAuth.addAuthStateListener(listener);
+
+            })
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        Completable.timer(3, TimeUnit.SECONDS,AndroidSchedulers.mainThread())
-            .subscribe({ Toast.makeText(this@SplashScreenActivity,"Splash Screen run done!",Toast.LENGTH_SHORT).show() })
+        init()
+
+    }
+    private fun init(){
+        providers = Arrays.asList(
+            AuthUI.IdpConfig.PhoneBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+        firebaseAuth = FirebaseAuth.getInstance()
+        listener  = FirebaseAuth.AuthStateListener { myFirebaseAuth ->
+            val user = myFirebaseAuth.currentUser
+            if(user !=null)
+                Toast.makeText(this@SplashScreenActivity, "welcome: "+user.uid,Toast.LENGTH_SHORT).show()
+            else
+                showLoginLayout()
+        }
+    }
+
+    private fun showLoginLayout() {
+        val authMethodPickerLayout = AuthMethodPickerLayout.Builder(R.layout.layout_sign_in)
+            .setPhoneButtonId(R.id.btn_phone_sign_in)
+            .setGithubButtonId(R.id.btn_google_sign_in)
+            .build();
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAuthMethodPickerLayout(authMethodPickerLayout)
+                .setTheme(R.style.LoginTheme)
+
+        )
+
     }
 }
