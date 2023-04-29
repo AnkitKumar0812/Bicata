47843ff3a6722f8514b48a209c8f8a08ae63b190
Ankit gupta <ankitkumar89692@gmail.com>
2023-04-29 22:06:28 +0530

    29/04/2023

--- app/src/main/java/com/example/bicatakotline/SplashScreenActivity.kt
+++ app/src/main/java/com/example/bicatakotline/SplashScreenActivity.kt
@@ -1,10 +1,13 @@
 package com.example.bicatakotline
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.firebase.ui.auth.AuthMethodPickerLayout
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.android.gms.auth.api.Auth
 import com.google.firebase.auth.FirebaseAuth
 import io.reactivex.Completable
@@ -76,5 +79,19 @@ class SplashScreenActivity : AppCompatActivity() {
 
 
 
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode== LOGIN_REQUEST_CODE){
+            val response = IdpResponse.fromResultIntent(data)
+            if(resultCode == Activity.RESULT_OK)
+            {
+                val user = FirebaseAuth.getInstance().currentUser
+            }
+            else
+                Toast.makeText(this@SplashScreenActivity,""+response!!.error!!.message,Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
