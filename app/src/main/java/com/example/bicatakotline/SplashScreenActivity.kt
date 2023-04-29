f1a52669f31da4a7fdd515430a1f3adf245791ad
Ankit gupta <ankitkumar89692@gmail.com>
2023-04-29 13:18:42 +0530

    29/04/2023

---
+++ app/src/main/java/com/example/bicatakotline/SplashScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bicatakotline
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
